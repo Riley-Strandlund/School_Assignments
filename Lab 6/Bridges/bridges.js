9904034f0c4c2fc81aba7efd6245c05c594cb035
Riley-Strandlund <FFOrstrandlund@gmail.com>
2023-09-28 01:12:29 -0500

    Changes

---
+++ Lab 6/Bridges/bridges.js
@@ -0,0 +1,31 @@
+let bridgeData = [
+    {name: 'Verrazano-Narrows Bridge',
+    cityState: 'New York, NY',
+    span: 1298.4,
+    location: [48.6866, -74.0447]
+    },
+    {name: 'Golden Gate Bridge',
+    cityState: 'San Francisco and Marin, CA',
+    span: 1280.2,
+    location: [37.8199, -122.4783]
+    },
+    {name: 'Mackinac Bridge',
+    cityState: 'Mackinaw and St Ignace, MI',
+    span: 1158.0,
+    location: [45.8174, -84.7278]
+    },
+    {name: 'George Washington Bridge',
+    cityState: 'New York, NY and New Jersey, NJ',
+    span: 1067.0,
+    location: [40.8517, -73.9527]
+    },
+    {name: 'Tacoma Narrows Bridge',
+    cityState: 'Tacoma and Kitsap, WA',
+    span: 853.44,
+    location: [47.2690, -122.5517]
+    }
+    //ToDo create other bridge objects
+
+    //create a map - like in videos
+    // loop over your bridge data array.
+]
