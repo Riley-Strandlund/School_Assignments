4e064c7c33b837fbbba324607b19bbdde1457687
Riley-Strandlund <FFOrstrandlund@gmail.com>
2023-11-10 03:03:47 -0600

    Lab 10

---
+++ Lab 10/would-you-rather/src/App.vue
@@ -0,0 +1,55 @@
+Parent
+Styles will be applied to children as well.
+<script setup>//setup replaces the setup from earlier labs
+import WouldYouRather from './components/WouldYouRather.vue'
+
+import { ref } from 'vue'
+
+const wyrQuestion = ref('Live in a house shaped like a triangle or a house shaped like a circle')
+const wyrAnswer1 = ref('Triangle house')
+const wyrAnswer2 = ref('Circle house')
+
+// this will store the user's answer once they make a choice
+
+const userSelection = ref('')
+
+function updateUserSelection(userChoice) {
+  userSelection.value = `Thanks! You chose ${userChoice}`
+}
+
+</script>
+
+<template><!-- Template isn't JavaScript but Vue specific--><!-- Template at least 'partially' replaces the <div id="app"> -->
+ 
+ <div id="app-component">
+
+    <h1>Would You Rather?</h1>
+
+    <WouldYouRather 
+    v-bind:question="wyrQuestion" 
+    v-bind:answer1="wyrAnswer1" 
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-selected="updateUserSelection"
+    ></WouldYouRather><!--The string can be anything even if it is manually typed in it-->
+
+    <p>{{ userSelection }}</p>
+
+ </div>
+
+
+</template>
+
+<style scoped>
+
+#app-component {
+  font-size: 1em;
+  background-color: aqua;
+  padding: 40px;
+}
+
+p {
+  font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif
+}
+
+
+</style>
---
+++ Lab 10/would-you-rather/src/components/WouldYouRather.vue
@@ -0,0 +1,59 @@
+Child
+emits events when new data is revcieved
+styles only apply to their component or each child file never parent
+<script setup>//Earlier labs we had to return variables, with vite the build returns it for you.
+import { ref, watch } from 'vue'
+
+defineProps({//"My parent component will tell me what these are. I'll get told what they are."
+  question: String,
+  answer1: String,
+  answer2: String
+})
+
+const emit = defineEmits([
+  'answer-selected'
+])
+
+const choice = ref('')
+
+/*function choiceMade() { 
+  // emit event tot tell parent that user has made choice.
+  emit('answer-selected', choice.value)
+} 1st way to do this*/
+
+//watch() watches the choice variable then emits when choice is made
+watch(choice, () => {
+  emit('answer-selected', choice.value)//function returns another function emit(function-name, dataToSend)
+})
+
+</script>
+
+<template>
+
+<div id="wyr">
+  <h2>Make Your Choice!</h2>
+
+  <p>{{  question }}</p>
+
+  <div>
+    <input v-model="choice" v-bind:value="answer1" type="radio" id="answer-1"> <!--1st way, add v-on:change="choiceMade"-->
+    <label for="answer-1">{{ answer1 }}</label>
+
+    <input v-model="choice" v-bind:value="answer2" type="radio" id="answer-2"><!-- 1st way, add v-on:change="choiceMade"-->
+    <label for="answer-2">{{ answer2 }}</label>  
+  </div>
+</div>
+
+</template>
+
+<style scoped>
+#wyr {
+  background-color: lightblue;
+  padding: 2%;
+  font-size: 15px;
+}
+p {
+  font-family: fantasy;
+}
+
+</style>
